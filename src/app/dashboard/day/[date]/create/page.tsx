a823ccb06f9ef55f918fc83462078d7853dea47d
Alex Castillo <alex.jc7516@gmail.com>
2024-02-22 18:19:18 -0700

    purge of unused actions

--- src/app/dashboard/day/[date]/create/page.tsx
+++ src/app/dashboard/day/[date]/create/page.tsx
@@ -1,10 +1,4 @@
 import { DayForms } from "~/components/day";
-import { ExerciseFormV2 } from "~/components/forms/day/exercise";
-import { HealthFormV2 } from "~/components/forms/day/health";
-import { MiscForm } from "~/components/forms/day/misc";
-import { SleepFormV2 } from "~/components/forms/day/sleep";
-import { StressForm } from "~/components/forms/day/stress";
-import { SupplementFormV2 } from "~/components/forms/day/supplements";
 
 export default function Page({ params }: { params: { date: string } }) {
   // create a day based on the date and the user id
